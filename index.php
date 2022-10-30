1b93fcf154b29c20fc106702d57ad303b3a470fc
Ryaz Pramesti <ryazpramesti122@gmail.com>
2022-10-30 23:04:01 +0700

    initial commit

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+
+$nama = "Ryaz Pramesti Dyah Anggraeni";
+$kontak = "Informasi Kontak";
+$pendidikan = "Pendidikan";
+$TahunSMA = "2017-2020";
+$TahunKuliah = "2020-Sekarang";
+$Instagram = "Instagram";
+$WhatsApp = "WhatsApp";
+$Email = "Email";
+
+echo "<h1><strong>$nama</strong></h1><br>";
+
+echo "Seorang mahasiswa yang sedang fokus mengembangkan diri di bidang konten kreatif dan media sosial. Aktif menambah pengalaman di organisasi, kepanitiaan dan berbagai proyek dosen maupun kampus. Memiliki motivasi berkembang yang tinggi dan mampu bekerja.<br>";
+
+echo "<h2><strong>$pendidikan</strong></h2>";
+echo "<li><strong>$TahunSMA</strong> SMA Negeri 3 Kediri</li>";
+echo "<li><strong>$TahunKuliah</strong> Universitas Negeri Surabaya</li>";
+
+echo "<h2><strong>$kontak</strong></h2>";
+echo "<li><strong>$Instagram</strong> : @rzprmsdyanggrn_</li>";
+echo "<li><strong>$WhatsApp</strong> :  085608425363</li>";
+echo "<li><strong>$Email</strong> :  ryazpramesti122@gmail.com</li>";
+
+?>
